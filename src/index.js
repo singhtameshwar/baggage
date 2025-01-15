07b5cfb06e1ecfb3bc56cccf2d21397567d32954
singhtameshwar <159401216+singhtameshwar@users.noreply.github.com>
2025-01-15 13:33:54 +0530

    adding new code

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Navbar from './components/navbar/page';  // Import Navbar component
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Navbar />  
+    <App />    
   </React.StrictMode>
 );
 
